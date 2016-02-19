f658d412173967c4bf26c295f33c1d80aad372a4
Zanc <zanc.razvan@gmail.com>
2016-02-19 18:25:44 +0200

    added spring

---
+++ src/main/java/com/licenta/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.licenta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+
+		return "/WEB-INF/jsp/index.jsp";
+	}
+
+}
