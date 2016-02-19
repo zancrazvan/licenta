4659b9afa41afca504e77270b01ac7f04cff2406
Zanc <zanc.razvan@gmail.com>
2016-02-19 19:05:28 +0200

    added tiles

--- src/main/java/com/licenta/controller/IndexController.java
+++ src/main/java/com/licenta/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 
-		return "/WEB-INF/jsp/index.jsp";
+		return "index";
 	}
 
 }
