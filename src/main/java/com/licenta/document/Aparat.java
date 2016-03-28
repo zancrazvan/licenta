2bd81ac1faf08255da1334e884ff0ecdb60e42f2
Razvan <zanc.razvan@gmail.com>
2016-03-28 13:31:17 +0300

    a

---
+++ src/main/java/com/licenta/document/Aparat.java
@@ -0,0 +1,25 @@
+package com.licenta.document;
+
+public class Aparat {
+
+	private int id;
+
+	private float putere;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public float getPutere() {
+		return putere;
+	}
+
+	public void setPutere(float putere) {
+		this.putere = putere;
+	}
+
+}
---
+++ src/main/java/com/licenta/service/ZancAlgorithm.java
@@ -0,0 +1,5 @@
+package com.licenta.service;
+
+public class ZancAlgorithm {
+
+}
