9b93075ee29df0053356b615438a4a9d44eb65c9
gheorgheciceu <gh.ciceu@gmail.com>
2016-03-28 13:35:14 +0300

    c

--- src/main/java/com/licenta/component/DSOGenerator.java
+++ src/main/java/com/licenta/component/DSOGenerator.java
@@ -64,4 +64,36 @@ public class DSOGenerator {
 			// report
 		}
 	}
+	public void generateDSOCsv(String filename, int minConsumption,int max) {
+
+		File file = new File(filename);
+		try {
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fileWritter = new FileWriter(file.getName(), false);
+			BufferedWriter writer = new BufferedWriter(fileWritter);
+
+			Date d = new Date();
+
+			d.setHours(0);
+			d.setSeconds(0);
+			int current;
+			for (int i = 0; i < 60 * 60 * 24; i++) {
+				TimeSeriesBit t = new TimeSeriesBit();
+				t.setDate(d);
+				t.setValue(minConsumption);
+				d.setSeconds(d.getSeconds() + 1);
+				int val = minConsumption + ((i / 3600) % 12) * 50;
+				System.out.println(((i / 3600) % 12) * 50);
+				writer.write(d.toGMTString() + "," + val);
+				writer.write("\r\n");
+
+			}
+
+			writer.close();
+		} catch (IOException ex) {
+			// report
+		}
+	}
 }
