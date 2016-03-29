40a05c61263fc497e9c57e66eb6815d00e7b8163
Razvan <zanc.razvan@gmail.com>
2016-03-30 00:19:45 +0300

    aa

--- src/main/java/com/licenta/service/ZancAlgorithm.java
+++ src/main/java/com/licenta/service/ZancAlgorithm.java
@@ -1,6 +1,8 @@
 package com.licenta.service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,22 +13,36 @@ public class ZancAlgorithm {
 
 	public void zancAlgorithm(House house, Map<Date, Float> dso) {
 
-		Map<Float, List<Aparat>> plan;
+		Map<Date, List<Aparat>> plan = new HashMap<>();
 
 		for (Date date : dso.keySet()) {
 
 			float putereDSO = dso.get(date);
 			List<Aparat> aparate = getAparatByPutere(putereDSO, house);
-			
-			
+
+			for (Aparat aparat : aparate) {
+
+			}
 
 		}
 
-	}
+	}  
 
 	public static List<Aparat> getAparatByPutere(float putere, House house) {
 
-		return house.getAparate().get(putere);
+		if (house.getAparate().containsKey(putere)) {
+
+			return house.getAparate().get(putere);
+		}
+
+		return null;
+	}
+
+	public static List<Aparat> getAparateCuPutereMaiMica(float putere, House house) {
+
+		// toate permutarile de aparate a caror suma puterilor < @putere
+
+		return null;
 	}
 
 }
